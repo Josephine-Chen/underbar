9b1b8dc1e45cbad35034001393d06681d2f162f4
Josephine Chen <jychen@g.hmc.edu>
2016-11-23 02:27:55 -0800

    all test cases passed for filter

--- src/underbar.js
+++ src/underbar.js
@@ -88,6 +88,13 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var trueArr = [];
+    for(var i = 0;i<collection.length;i++){
+      if(test(collection[i])){
+        trueArr.push(collection[i]);
+      }
+    }
+    return trueArr;
   };
 
   // Return all elements of an array that don't pass a truth test.
