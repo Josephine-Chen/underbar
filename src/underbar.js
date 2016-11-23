12d447b03dffe55e7ecf6f4ff6ef065a430bb2f4
Josephine Chen <jychen@g.hmc.edu>
2016-11-23 02:24:58 -0800

    all test cases passed for each

--- src/underbar.js
+++ src/underbar.js
@@ -59,12 +59,12 @@
   _.each = function(collection, iterator) {
     if(Array.isArray(collection)){
       for(var i =0;i<collection.length;i++){
-        iterator(collection[i]);
+        iterator(collection[i],i,collection);
       }
     }
     else{
       for(var key in collection){
-        iterator(collection[key]);
+        iterator(collection[key],key,collection);
       }
     }
   };
